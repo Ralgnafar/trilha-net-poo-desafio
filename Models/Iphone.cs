7184917cb21fa43eef5ac9316cfa9f9192d57b14
Ericsson Almeida <101663642+Ralgnafar@users.noreply.github.com>
2022-10-25 19:18:20 -0300

    Update Iphone.cs

--- Models/Iphone.cs
+++ Models/Iphone.cs
@@ -1,8 +1,22 @@
 namespace DesafioPOO.Models
 {
-    // TODO: Herdar da classe "Smartphone"
-    public class Iphone
+    public class Iphone : Smartphone
     {
-        // TODO: Sobrescrever o método "InstalarAplicativo"
+        // Passa os parâmetros recebidos nessa classe para a classe pai
+        public Iphone(string numero, string modelo, string imei, int memoria) : base(numero, modelo, imei, memoria) {}
+
+        public override void InstalarAplicativo(string nomeApp)
+        {
+            if(VerificarAplicativo(nomeApp))
+            {
+                Console.WriteLine($"Instalando o aplicativo \"{nomeApp}\" no iPhone");
+            }
+        }
+
+        // Método selado, ele não pode ser sobrescrito nas classes filhas
+        public sealed override void ExibirConfiguracoes()
+        {
+            Console.WriteLine($"As configurações do iPhone {Modelo} são: {Memoria} gigas de armazenamento e IMEI: {IMEI}");
+        }
     }
 }
