d8d5fd47f1b576e8156b926dd7a7dca161e51f3c
Ericsson Almeida <101663642+Ralgnafar@users.noreply.github.com>
2022-10-25 19:18:49 -0300

    Update Nokia.cs

--- Models/Nokia.cs
+++ Models/Nokia.cs
@@ -1,8 +1,22 @@
 namespace DesafioPOO.Models
 {
-    // TODO: Herdar da classe "Smartphone"
-    public class Nokia
+    public class Nokia : Smartphone
     {
-        // TODO: Sobrescrever o método "InstalarAplicativo"
+        // Passa os parâmetros recebidos nessa classe para a classe pai
+        public Nokia(string numero, string modelo, string imei, int memoria) : base(numero, modelo, imei, memoria) {}
+
+        public override void InstalarAplicativo(string nomeApp)
+        {
+            if(VerificarAplicativo(nomeApp))
+            {
+                Console.WriteLine($"Instalando o aplicativo \"{nomeApp}\" no Nokia");
+            }
+        }
+
+        // Método selado, ele não pode ser sobrescrito nas classes filhas
+        public sealed override void ExibirConfiguracoes()
+        {
+            Console.WriteLine($"As configurações do Nokia {Modelo} são: {Memoria} gigas de armazenamento e IMEI: {IMEI}");
+        }
     }
 }
